ce8e0a4346bc18a696f3152576928aeae95618fc
Adam Ruggles <a.ruggles@gmail.com>
2014-07-23 21:21:30 -0700

    Updated code to update navigation

--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,49 @@ app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
 		controller: 'setupController',
 		templateUrl: 'views/setup.html'
+	}).when('/predict', {
+		controller: 'predictController',
+		templateUrl: 'views/predict.html'
 	})
 	.otherwise({ redirectTo: '/'});
 
 }]);
 
 
-app.controller('setupController', ['$scope',
-	function($scope) {
-
+app.controller('setupController', ['$scope', 'navigationFactory',
+	function($scope, navigationFactory) {
+		navigationFactory.setName('Setup');
+		navigationFactory.setLink('/#');
 	}
+])
+app.controller('predictController', ['$scope', 'navigationFactory',
+	function($scope, navigationFactory) {
+		navigationFactory.setName('Predict');
+		navigationFactory.setLink('/#predict');
+	}
+]).controller('navigationController', ['$scope', 'navigationFactory',
+	function($scope, navigationFactory) {
+		var nav = {
+			"name": navigationFactory.getName(),
+			"link": navigationFactory.getLink()
+		}
+		$scope.navigation = nav;
 
+		$scope.$watch(function() { return navigationFactory.getName(); },
+			function (value) {
+				if (value) {
+					$scope.navigation.name = value;
+				}
+			}
+		);
+		$scope.$watch(function() { return navigationFactory.getLink(); },
+			function (value) {
+				if (value) {
+					$scope.navigation.link = value;
+				}
+			}
+		);
+	}
 ]);
 
 app.directive('robotManager', function() {
@@ -23,6 +55,24 @@ app.directive('robotManager', function() {
 		restrict: 'E',
 		link: function(scope, elem) {
 			return elem.replaceWith(Linkbots.managerElement());
+		}
+	}
+});
+
+app.factory('navigationFactory', function() {
+	var data = { name: 'Setup', link: '/#'};
+	return {
+		getName: function() {
+			return data.name;
+		},
+		setName: function(newName) {
+			data.name = newName;
+		},
+		getLink: function() {
+			return data.link;
+		},
+		setLink: function(newLink) {
+			data.link = newLink;
 		}
 	}
 });
